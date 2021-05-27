ff1e3ac7fa04d58940560eb7f7996a15a0423149
Nikita Isaev <keller108@yandex.ru>
2021-05-27 23:11:24 +0500

    react/ PopupWithForm in process

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,9 +1,24 @@
-function PopupWithForm () {
+function PopupWithForm ({title, name}) {
     return (
         <div className="popup popup_type_profile">
-                <div className="popup__container">
-                <button className="popup__close-btn hover-transparency" type="button" aria-label="Close" />
-                </div>
+             <div className="popup__container">
+                <button 
+                    className={`popup popup_type_${props.name}`}
+                    type="button" 
+                    aria-label="Close">
+                </button>
+                <h2 className="popup__title">
+                    {title}
+                </h2>
+                <form 
+                    className={`form ${props.name}`}
+                    name="profile-popup"
+                    id="form-profile"
+                    noValidate>
+                </form>
             </div>
+        </div>
     )
 }
+
+export default PopupWithForm;
